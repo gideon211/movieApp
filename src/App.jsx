0c76296dd2fb2ac4435f07fea46213dfc6ddeccf
Gideon <kwarteon08@gmail.com>
2025-06-29 20:48:29 +0000

    app.jsx finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
 }
 
-export default App
+export default App;
